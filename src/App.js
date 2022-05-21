1e7aae1a406338f45da7f407c4cbb64a199c2452
Sergio S <TheLastRKoch@gmail.com>
2022-05-20 23:01:37 -0600

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Container, Button} from '@material-ui/core'; 
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Button variant="contained">This is a init</Button>
+    </Container>
+  );
+}
+
+export default App;
