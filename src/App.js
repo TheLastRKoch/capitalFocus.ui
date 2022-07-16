a8d6ca2545536557e636e57c7e390dffcf889370
Sergio S <TheLastRKoch@gmail.com>
2022-07-16 16:56:52 -0600

    working the the base ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import BudgetDetail from './components/BudgetDetail/BudgetDetail';
-import BudgetElement from './components/BudgetElement/BudgetElement'
-import ThemeProvider from 'react-bootstrap/ThemeProvider'
-import Temp from './components/Temp/Temp';
-
 function App() {
   return (
-    // <Container>
-    //   <BudgetElement categoryName="Luxuries" amount="20.000" remainingAmount="10.000"></BudgetElement>
-    //   <BudgetDetail description="Cinema" amount="12.000"></BudgetDetail>
-    //   <BudgetElement categoryName="Groseries" amount="70.000" remainingAmount="10.000"></BudgetElement>
-    //   <BudgetElement categoryName="Travel" amount="30.000" remainingAmount="10.000"></BudgetElement>
-    // </Container>
-    <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-    minBreakpoint="xxs">
-      <Temp></Temp>
-    </ThemeProvider>
+    "Hello Word"
   );
 }
+
 
 export default App;
